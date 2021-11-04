9cc0fe50764ac609e99d3228a88dfc4f7599a758
faruqismael <farusmith5@gmail.com>
2021-11-04 05:45:21 -0700

    simple footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./footer.css";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <footer>
+      <p className="">@copyright {new Date().getFullYear()}</p>
+    </footer>
+  );
 }
 
 export default Footer;
